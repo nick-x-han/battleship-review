8ebc8ab44959e39e6b53a4872dc01fb1fd0a4666
nickxhan <nickxhan@umich.edu>
2026-09-10 15:12:39 -0400

    Implement Ship class

---
+++ src/ship.js
@@ -0,0 +1,27 @@
+class Ship {
+  #length;
+  #hits;
+  #sunk;
+
+  constructor(length) {
+    this.#length = length;
+    this.#hits = 0;
+    this.#sunk = false;
+  }
+
+  hit() {
+    this.#hits++;
+    if (this.#hits === this.#length)
+      this.#sunk = true;
+  }
+
+  isSunk() {
+    return this.#sunk;
+  }
+
+  getLength() {
+    return this.#length;
+  }
+}
+
+export { Ship };
