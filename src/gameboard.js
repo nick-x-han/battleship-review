b1aee1c6b1cf811369f5aaa88f98bb863c55a110
nickxhan <nickxhan@umich.edu>
2026-09-11 21:41:35 -0400

    Finish tests and implementation for Gameboard.placeShip()

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,16 +1,5 @@
 const BOARD_SIZE = 10;
-const FLEET_ONE = 4;
-const FLEET_TWO = 3;
-const FLEET_THREE = 2;
-const FLEET_FOUR = 1;
-
-function checkBounds(origin, isVertical, length) {
-  let bound = isVertical ? origin[0] : origin[1];
-}
-
-function validateShipLength() {
-
-}
+const FLEET_SIZE = { 1: 4, 2: 3, 3: 2, 4: 1 };
 
 class Gameboard {
   #board;
@@ -19,20 +8,69 @@ class Gameboard {
   constructor() {
     this.#board = [];
     this.#ships = [];
-    this.#fleetCounts = { "1": 0, "2": 0, "3": 0, "4": 0 };
+    this.#fleetCounts = { 1: 0, 2: 0, 3: 0, 4: 0 };
     for (let i = 0; i < BOARD_SIZE; i++) {
       this.#board.push(new Array(BOARD_SIZE).fill(0, 0, BOARD_SIZE));
     }
   }
 
+  #validateFleetCount(length) {
+    if (FLEET_SIZE[length] > this.#fleetCounts[length]) {
+      return true;
+    }
+    return false;
+  }
+
+  #willOverlap(ship, origin, isVertical) {
+    for (let i = 0; i < ship.getLength(); i++) {
+      if (isVertical) {
+        if (this.#board[origin[0] + i][origin[1]] !== 0) {
+          return true;
+        }
+      } else {
+        if (this.#board[origin[0]][origin[1] + i] !== 0) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  #checkBounds(ship, origin, isVertical) {
+    if (
+      origin[0] < 0 ||
+      origin[1] < 0 ||
+      origin[0] >= BOARD_SIZE ||
+      origin[1] >= BOARD_SIZE
+    )
+      return false;
+
+    let vertical = isVertical ? 1 : 0;
+    let horizontal = isVertical ? 0 : 1;
+    if (origin[0] + ship.getLength() * vertical > BOARD_SIZE) return false;
+    if (origin[1] + ship.getLength() * horizontal > BOARD_SIZE) return false;
+    return true;
+  }
+
   placeShip(ship, origin, isVertical) {
-    if (!checkBounds(origin, isVertical, ship.getLength())) {
-      throw new Error("Cannot place out-of-bounds ship");
+    if (ship.getLength() > 4) {
+      throw new Error("Cannot place ships with length > 4");
     }
     if (this.#ships.includes(ship)) {
       throw new Error("Already placed this ship");
     }
+    if (!this.#checkBounds(ship, origin, isVertical)) {
+      throw new Error("Cannot place out-of-bounds ship");
+    }
+    if (!this.#validateFleetCount(ship.getLength())) {
+      throw new Error(`Exceeded limit of ships of length ${ship.getLength()}`);
+    }
+    if (this.#willOverlap(ship, origin, isVertical)) {
+      throw new Error("Cannot overlap onto another ship");
+    }
+
     this.#ships.push(ship);
+    this.#fleetCounts[ship.getLength()]++;
 
     for (let i = 0; i < ship.getLength(); i++) {
       if (isVertical) {
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -11,18 +11,80 @@ test("ship of length 4 placed at (3, 3) vertically goes to (6, 3)", () => {
   expect(gameboard.board[7][3]).toBe(0);
 })
 
-test("ship of length 4 cannot be placed at (8, 3)", () => {
+test("vertical ship of length 4 throws error if placed at (7, 3)", () => {
   let gameboard = new Gameboard();
   let ship = new Ship(4);
-  gameboard.placeShip(ship, [3, 3], true);
-  expect(gameboard.board[3][3]).toBe(ship);
+  expect(() => gameboard.placeShip(ship, [7, 3], true)).toThrow("Cannot place out-of-bounds ship");
 })
 
-//cannot place more than 1 four length
-//cannot place more than 2 three lengths
-//two different ships are treated separately
-//cannot place the same ship twice
+test("horizontal ship of length 4 throws error if placed at (3, 7)", () => {
+  let gameboard = new Gameboard();
+  let ship = new Ship(4);
+  expect(() => gameboard.placeShip(ship, [3, 7], false)).toThrow("Cannot place out-of-bounds ship");
+})
 
+test("x coordinate of origin cannot be out of bounds", () => {
+  let gameboard = new Gameboard();
+  let ship = new Ship(4);
+  expect(() => gameboard.placeShip(ship, [10, 0], true)).toThrow("Cannot place out-of-bounds ship");
+})
+
+test("y coordinate of origin cannot be out of bounds", () => {
+  let gameboard = new Gameboard();
+  let ship = new Ship(4);
+  expect(() => gameboard.placeShip(ship, [0, 10], true)).toThrow("Cannot place out-of-bounds ship");
+})
+
+test("vertical ship throws error if y coordinate is out of bounds", () => {
+  let gameboard = new Gameboard();
+  let ship = new Ship(4);
+  expect(() => gameboard.placeShip(ship, [0, 10], true)).toThrow("Cannot place out-of-bounds ship");
+})
+
+test("cannot place a ship with length > 4", () => {
+  let gameboard = new Gameboard();
+  let ship = new Ship(5);
+  expect(() => gameboard.placeShip(ship, [3, 3], true)).toThrow("Cannot place ships with length > 4");
+})
+
+test("cannot place the same ship twice", () => {
+  let gameboard = new Gameboard();
+  let ship = new Ship(4);
+  gameboard.placeShip(ship, [5, 5], true);
+  expect(() => gameboard.placeShip(ship, [0, 0], true)).toThrow("Already placed this ship");
+})
+
+test("cannot place more than one 4-length ship", () => {
+  let gameboard = new Gameboard();
+  let ship = new Ship(4);
+  let ship2 = new Ship(4);
+  gameboard.placeShip(ship, [3, 3], true);
+  expect(() => gameboard.placeShip(ship2, [3, 3], true)).toThrow("Exceeded limit of ships of length 4");
+})
+
+test("cannot place more than three 2-length ships", () => {
+  let gameboard = new Gameboard();
+  let ship = new Ship(2);
+  let ship2 = new Ship(2);
+  let ship3 = new Ship(2);
+  let ship4 = new Ship(2);
+  gameboard.placeShip(ship, [0, 0], true);
+  gameboard.placeShip(ship2, [0, 1], true);
+  gameboard.placeShip(ship3, [0, 2], true);
+  expect(() => gameboard.placeShip(ship4, [0, 3], true)).toThrow("Exceeded limit of ships of length 2");
+})
+
+test("cannot overlap two ships", () => {
+  let gameboard = new Gameboard();
+  let ship = new Ship(3);
+  let ship2 = new Ship(3);
+  gameboard.placeShip(ship, [0, 0], true);
+  expect(() => gameboard.placeShip(ship2, [2, 0], true)).toThrow("Cannot overlap onto another ship");
+})
+
+//can delete a ship with hits()
+  // 1 for successful hit, -1 for not
+//two different ships are treated separately
 
 
 
