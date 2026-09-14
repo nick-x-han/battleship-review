5dc8e7a27a2a5f15325adacbc58307dd40f7b1c9
nickxhan <nickxhan@umich.edu>
2026-09-13 21:55:32 -0400

    Prepare for new branch to make two humans possible

--- src/display.js
+++ src/display.js
@@ -15,9 +15,13 @@ export function playGame() {
   const humanBoardDiv = renderBoard(human);
   const cpuBoardDiv = renderBoard(cpu, true);
 
-  cpuBoardDiv.addEventListener("click", (e) => onClickEnemyBoard(e, human));
+  cpuBoardDiv.onclick = (e) => onClickEnemyBoard(e, human);
 
   contentDiv.append(humanBoardDiv, cpuBoardDiv);
+}
+
+function playTurn(player, isCPU) {
+  if (isCPU) cpuMove();
 }
 
 function renderBoard(player, isCPU) {
@@ -56,15 +60,17 @@ function appendAndReplaceCells(player, parent, isCPU) {
   }
 }
 
-function onClickEnemyBoard(e, cpu) {
+function onClickEnemyBoard(e, enemy) {
   let x = e.target.dataset.row;
   let y = e.target.dataset.column;
-  cpu.board.receiveAttack([x, y]);
+  enemy.board.receiveAttack([x, y]);
 
-  appendAndReplaceCells(cpu, e.currentTarget, true);
+  appendAndReplaceCells(enemy, e.currentTarget, true);
+  playTurn(enemy, true);
+  e.currentTarget.onclick = null;
 }
 
-function enemyMove() {}
+function cpuMove() {}
 
 
 
@@ -72,3 +78,9 @@ function enemyMove() {}
 //having a form, and each its own confirm
 //button to make checking overlaps easier
 //OR just show only human board 
+
+//random placements function; used for both cpu and also
+//is an option for player if needed
+
+//player class needs CPU argument for easy access and 
+//simplify arguments here
